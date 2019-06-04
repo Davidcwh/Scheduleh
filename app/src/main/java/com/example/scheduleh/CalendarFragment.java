7dac400f9ccd75abfc683f3aaba3e90de96b0103
davidcwh <dcwh12321@gmail.com>
2019-06-05 00:48:11 +0800

    Changed to collapsible calendar view

--- app/src/main/java/com/example/scheduleh/CalendarFragment.java
+++ app/src/main/java/com/example/scheduleh/CalendarFragment.java
@@ -4,15 +4,18 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.applikeysolutions.cosmocalendar.view.CalendarView;
+
+import com.shrikanthravi.collapsiblecalendarview.widget.CollapsibleCalendar;
+import com.shrikanthravi.collapsiblecalendarview.data.Day;
 
 
 public class CalendarFragment extends Fragment {
 
-    CalendarView calendarView;
+    CollapsibleCalendar collapsibleCalendar;
 
     @Nullable
     @Override
@@ -20,4 +23,38 @@ public class CalendarFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_calendar, null);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        collapsibleCalendar = getView().findViewById(R.id.collapsibleCalendarView);
+        collapsibleCalendar.setCalendarListener(new CollapsibleCalendar.CalendarListener() {
+            @Override
+            public void onDaySelect() {
+                Day day = collapsibleCalendar.getSelectedDay();
+                Log.i(getClass().getName(), "Selected Day: "
+                        + day.getYear() + "/" + (day.getMonth() + 1) + "/" + day.getDay());
+            }
+
+            @Override
+            public void onItemClick(View v) {
+
+            }
+
+            @Override
+            public void onDataUpdate() {
+
+            }
+
+            @Override
+            public void onMonthChange() {
+
+            }
+
+            @Override
+            public void onWeekChange(int position) {
+
+            }
+        });
+
+    }
 }
